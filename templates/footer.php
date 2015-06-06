c94e75059541f95b2367b663a31853e876a9e4ff
Craig Mod <stuff@craigmod.com>
2015-06-06 11:30:24 +0900

    date

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,6 @@
     <div class="container">
       <footer class="row">
-        Art Space Tokyo &copy; 2008-2014 <a href="http://prepostbooks.com/">PRE/POST</a> <span class="follow"><a href="mailto:yes@prepostbooks.com">yes@prepostbooks.com</a> | <a href="http://twitter.com/artspacetokyo/">@artspacetokyo</a> | <a href="http://facebook.com/artspacetokyo/">Facebook</a></span>
+        Art Space Tokyo &copy; 2008-<?php echo date("Y") ?> <a href="http://prepostbooks.com/">PRE/POST</a> <span class="follow"><a href="mailto:yes@prepostbooks.com">yes@prepostbooks.com</a> | <a href="http://twitter.com/artspacetokyo/">@artspacetokyo</a> | <a href="http://facebook.com/artspacetokyo/">Facebook</a></span>
       </footer>
     </div>
     <script src="http://artspacetokyo.com/mint/?js" type="text/javascript"></script>
