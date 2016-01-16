c67bebc0d70df5b0ff2d4eaddb3215808371870f
Craig Mod <stuff@craigmod.com>
2016-01-16 12:22:09 +0900

    download from us

--- templates/saleslinks.php
+++ templates/saleslinks.php
@@ -3,6 +3,7 @@
   $amazonlink = 'http://www.amazon.com/gp/product/B008Y3MWY2/ref=as_li_ss_tl?ie=UTF8&camp=1789&creative=390957&creativeASIN=B008Y3MWY2&linkCode=as2&tag=cramod-20';
   $bnlink     = 'http://www.barnesandnoble.com/w/art-space-tokyo-ashley-rawlings/1009336806?ean=2940014892957';
   $ituneslink = 'http://itunes.apple.com/us/book/art-space-tokyo/id553115431?ls=1';
-  $bundlelink = 'http://prepost.fetchapp.com/sell/hxahraiz';
+  $bundlelink = 'http://read.artspacetokyo.com/downloads/art_space_tokyo.zip';
+  // $bundlelink = 'http://prepost.fetchapp.com/sell/hxahraiz';
   
  ?>
